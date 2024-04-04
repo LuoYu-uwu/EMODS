3f2b40ca9541280ee64d60614ae8e48ba7ba6abd
liuy1103 <e0968020@u.nus.edu>
2024-04-04 22:29:52 +0800

    Modify to reduce LUT

--- MODS/MODS.srcs/sources_1/new/Top_Student.v
+++ MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -44,6 +44,7 @@ module Top_Student (
     reg enable_closet;
     reg enable_bath;
     reg [2:0] activities;
+    reg [2:0] prev_todo = 3'b000;
     
     initial begin
         enable_home = 1;
@@ -151,7 +152,6 @@ module Top_Student (
             oled_data <= oled_data_home;
         end
     end
-
     
     Oled_Display unit_oled(.clk(clk_6p25m), .reset(0), .frame_begin(fb), 
     .sending_pixels(send), .sample_pixel(sample), .pixel_index(pixel_index), 
